27740a7e23eef112edef6513f7501ac877f694ef
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2025-01-13 00:37:10 +0330

    feat: add type for poker store

--- src/store/poker.ts
+++ src/store/poker.ts
@@ -1,17 +1,20 @@
-import { reactive } from 'vue'
-import type { GameState } from '@/types'
+import { reactive } from 'vue';
+import type { GameState, PokerStore } from '@/types';
 // using simple reactive as state management
 // TODO: for more advanced usage, it's better to use Pinia
 // https://vuejs.org/guide/scaling-up/state-management.html#simple-state-management-with-reactivity-api
 
-
-export const pokerStore = reactive({
-  gameState: 'game' as GameState,
+export const pokerStore = reactive<PokerStore>({
+  gameState: 'start',
   timeLeft: 100,
+  attemptsResults: [],
   setTimeLeft(time: number) {
-    this.timeLeft = time
+    this.timeLeft = time;
   },
   setPokerState(state: GameState) {
-    this.gameState = state
-  }
-})
+    this.gameState = state;
+  },
+  addAttemptResult(attemptCorrectAnswers: number) {
+    this.attemptsResults.push(attemptCorrectAnswers);
+  },
+});
