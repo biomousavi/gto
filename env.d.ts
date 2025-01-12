23ec4af477cd82327bdaa9f681e8c287d6fd2e57
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2025-01-13 01:25:18 +0330

    build: add pokersolver package types

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+declare module 'pokersolver' {
+  export class Hand {
+    static solve(cards: string[]): Hand;
+    name: string;
+    descr: string;
+    cards: string[];
+    rank: number;
+    toString(): string;
+  }
+}
