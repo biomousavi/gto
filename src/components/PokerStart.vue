1d9b5085c12384ef66ad0e5cfe513dc61d31effd
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2025-01-13 00:59:05 +0330

    feat: rename intro to start

---
+++ src/components/PokerStart.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { pokerStore } from '@/store/poker';
+
+function startGame() {
+  pokerStore.setPokerState('game')
+}
+</script>
+
+<template>
+  <div>
+    <h1>Guess the cards</h1>
+    <button @click="startGame">PLAY</button>
+  </div>
+
+</template>
+
+
+<style lang="css" scoped>
+div {
+  display: flex;
+  gap: 2rem;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+h1 {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
