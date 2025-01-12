19a2267376a53dcb20ccf3035f8ba3109a9734a8
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2025-01-12 18:54:11 +0330

    feat: add poker store types

--- src/types/poker.ts
+++ src/types/poker.ts
@@ -1,1 +1,10 @@
 export type GameState = 'intro' | 'game' | 'result' | 'end';
+
+export interface PokerStore {
+  gameState: GameState;
+  timeLeft: number;
+  attemptsResults: number[];
+  setTimeLeft(time: number): void;
+  setPokerState(state: GameState): void;
+  addAttemptResult(attemptCorrectAnswers: number): void;
+}
